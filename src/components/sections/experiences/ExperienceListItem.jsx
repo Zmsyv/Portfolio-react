f8a9716f9ba92e5ed08f12cddde36fb0e1127159
Zmsyv <msyvzhrb@gmail.com>
2024-07-31 18:22:52 +0400

    the positions in the experience section were fixed

--- src/components/sections/experiences/ExperienceListItem.jsx
+++ src/components/sections/experiences/ExperienceListItem.jsx
@@ -4,9 +4,13 @@ const ExperienceListItem = ({ item }) => {
   return (
     <div>
       <div className="bg-white rounded-[50%] h-[120px] w-[120px]">
-        <img className=" flex justify-center items-center" src={item.src} alt="" />
+        <img
+          className=" h-[75px] w-[75px] relative left-[23px] top-[22px]"
+          src={item.src}
+          alt=""
+        />
       </div>
-      <p className=" flex justify-center">{item.text}</p>
+      <p className=" flex justify-center text-white">{item.text}</p>
     </div>
   );
 };
--- src/components/sections/experiences/ExperienceMain.jsx
+++ src/components/sections/experiences/ExperienceMain.jsx
@@ -6,7 +6,7 @@ const ExperienceMain = () => {
   return (
     <>
       <div className=" pt-[70px] pl-[70px] ">
-        <strong className="text-[35px]">EXPERIENCE</strong>
+        <strong className="text-[35px] text-white">EXPERIENCE</strong>
       </div>
       <div className="flex justify-between pl-[70px] pr-[70px]">
         <ExperienceList />
